fa76242648ee459ef383e6d506cf72bf0a8ffa4d
Vinicius Mendite <vinicius@sharbe.com.br>
2021-06-16 16:47:20 -0300

    broadcast of logout

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,16 +1,13 @@
-import { destroyCookie } from "nookies";
 import { useContext, useEffect } from "react";
 import { Can } from "../components/Can";
 import { AuthContext } from "../contexts/AuthContext";
-import { useCan } from "../hooks/useCan";
 import { setupAPIClient } from "../services/api";
 
 import { api } from "../services/apiClient";
-import { AuthTokenError } from "../services/errors/AuthTokenError";
 import { withSSRAuth } from "../utils/withSSRAuth";
 
 export default function Dashboard() {
-  const { user } = useContext(AuthContext);
+  const { user, signOut } = useContext(AuthContext);
 
   // const userCanSeeMetrics = useCan({roles: ['metrics.list', 'administrator']})
 
@@ -22,6 +19,8 @@ export default function Dashboard() {
   return (
     <>
       <h1>Dashboard: {user?.email}</h1>
+
+      <button type="button" onClick={signOut}>Sign out</button>
 
       <Can permissions={['metrics.list']}>
         <div>Métricas</div>
