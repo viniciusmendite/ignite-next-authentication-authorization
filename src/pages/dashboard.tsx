5884e9c82715a9ca6c7e0a597d9951ee3d1206e1
Vinicius Mendite <vinicius@sharbe.com.br>
2021-06-16 15:52:45 -0300

    create component permission

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,5 +1,6 @@
 import { destroyCookie } from "nookies";
 import { useContext, useEffect } from "react";
+import { Can } from "../components/Can";
 import { AuthContext } from "../contexts/AuthContext";
 import { useCan } from "../hooks/useCan";
 import { setupAPIClient } from "../services/api";
@@ -11,7 +12,7 @@ import { withSSRAuth } from "../utils/withSSRAuth";
 export default function Dashboard() {
   const { user } = useContext(AuthContext);
 
-  const userCanSeeMetrics = useCan({roles: ['metrics.list', 'administrator']})
+  // const userCanSeeMetrics = useCan({roles: ['metrics.list', 'administrator']})
 
   useEffect(() => {
     api.get('/me').then(response => console.log(response.data))
@@ -20,9 +21,11 @@ export default function Dashboard() {
 
   return (
     <>
-    <h1>Dashboard: {user?.email}</h1>
-    
-    {userCanSeeMetrics && <div>Métricas</div>}
+      <h1>Dashboard: {user?.email}</h1>
+
+      <Can permissions={['metrics.list']}>
+        <div>Métricas</div>
+      </Can>
     </>
   );
 }
