399dcc12b7b6dcb53330b050fcf6438f954e4aed
Vinicius Mendite <vinicius@sharbe.com.br>
2021-06-16 15:42:12 -0300

    create hook permission

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,7 @@
 import { destroyCookie } from "nookies";
 import { useContext, useEffect } from "react";
 import { AuthContext } from "../contexts/AuthContext";
+import { useCan } from "../hooks/useCan";
 import { setupAPIClient } from "../services/api";
 
 import { api } from "../services/apiClient";
@@ -10,13 +11,19 @@ import { withSSRAuth } from "../utils/withSSRAuth";
 export default function Dashboard() {
   const { user } = useContext(AuthContext);
 
+  const userCanSeeMetrics = useCan({roles: ['metrics.list', 'administrator']})
+
   useEffect(() => {
     api.get('/me').then(response => console.log(response.data))
       .catch(err => console.log(err));
   }, [])
 
   return (
+    <>
     <h1>Dashboard: {user?.email}</h1>
+    
+    {userCanSeeMetrics && <div>Métricas</div>}
+    </>
   );
 }
 
